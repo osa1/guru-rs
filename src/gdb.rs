68625a3118a748d73083d58e63e364978ccbff6e
Ömer Sinan Ağacan <omeragacan@gmail.com>
2019-02-03 09:19:21 +0300

    Handle parsing multiple mi msgs in single read in the socket listener

--- src/gdb.rs
+++ src/gdb.rs
@@ -56,18 +56,12 @@ static MI_MSG_SEP: &'static str = "(gdb) \n";
 
 /// Read "(gdb)\n" delimited mi messages from `stdout`, send parsed messages to `msg_handler`.
 fn message_handler(stdout: &mut ChildStdout, msg_sender: &mut Sender<mi::Output>) {
-    // We can't do incremental parsing yet so collect output until we see a "(gdb)\n".
+    // We can't do incremental parsing yet so collect output until we see a "(gdb) \n".
     // We also can't search in a [u8] (nothing like str::find for other slices) so we try to first
     // convert the accumulated output to str.
     let mut msg_bytes = Vec::new();
     loop {
-        let mut read_buf: [u8; 10000] = [0; 10000];
-        let len = stdout.read(&mut read_buf).unwrap();
-        println!("Message handler read {} bytes", len);
-        if len == 0 {
-            return;
-        }
-        msg_bytes.extend_from_slice(&read_buf[0..len]);
+        // Try to parse the buffer first.
         let msg_str = match str::from_utf8(&msg_bytes) {
             Err(err) => {
                 // sigh so many hacks ...
@@ -81,11 +75,8 @@ fn message_handler(stdout: &mut ChildStdout, msg_sender: &mut Sender<mi::Output>
             Ok(str) => str,
         };
         match msg_str.find(MI_MSG_SEP) {
-            None => {
-                // println!("Can't find MI_MSG_SEP: {:?}", msg_str);
-                continue;
-            }
             Some(idx) => {
+                // Found a complete message, parse it.
                 let msg = &msg_str[0..idx];
                 match mi::parse_output(msg) {
                     None => {
@@ -97,6 +88,17 @@ fn message_handler(stdout: &mut ChildStdout, msg_sender: &mut Sender<mi::Output>
                         msg_bytes.drain(0..idx + MI_MSG_SEP.len());
                     }
                 }
+            }
+            None => {
+                // Read more from the socket
+                let mut read_buf: [u8; 10000] = [0; 10000];
+                let len = stdout.read(&mut read_buf).unwrap();
+                println!("Message handler read {} bytes", len);
+                if len == 0 {
+                    // TODO: Somehow signal socket closure
+                    return;
+                }
+                msg_bytes.extend_from_slice(&read_buf[0..len]);
             }
         }
     }
